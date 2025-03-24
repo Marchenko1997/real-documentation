b95f485359fceadf378f474e614e0db59fdd55c4
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-03-24 23:32:24 +0100

    set up layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import { Inter as FontSans } from "next/font/google";
+import { dark } from "@clerk/themes";
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "LiveDocs is a go-to collaborative docs editor for teams.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { colorPrimary: "#3371FF", fontSize: "16px" },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
