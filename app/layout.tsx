070f5d9503428ba6ce24615ecb4ef06c3b76bc82
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-08-18 18:19:27 +0300

    updating layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,45 @@ import { Inter as FontSans } from "next/font/google";
 import { dark } from "@clerk/themes";
 import "./globals.css";
 
+// const fontSans = FontSans({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+// });
+
+
+// export const metadata: Metadata = {
+//   title: "LiveDocs",
+//   description: "LiveDocs is a go-to collaborative docs editor for teams.",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <ClerkProvider
+//       appearance={{
+//         baseTheme: dark,
+//         variables: { colorPrimary: "#3371FF", fontSize: "16px" },
+//       }}
+//     >
+//       <html lang="en" suppressHydrationWarning>
+//         <body
+//           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//         >
+//           {children}
+//         </body>
+//       </html>
+//     </ClerkProvider>
+//   );
+// }
+
+
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
-
 
 export const metadata: Metadata = {
   title: "LiveDocs",
@@ -18,9 +52,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider
       appearance={{
@@ -29,10 +63,14 @@ export default function RootLayout({
       }}
     >
       <html lang="en" suppressHydrationWarning>
+        <head />
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
         >
-          {children}
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>
