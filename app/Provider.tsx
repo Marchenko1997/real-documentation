1cb5f1c25d220d6b39ce17921036fdc3554009ef
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-08-19 19:33:55 +0300

    set up authentification

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -4,6 +4,20 @@ import { ReactNode } from "react";
 import { getClerkUsers } from "@/lib/actions/user.actions";
 import { useUser } from "@clerk/nextjs";
 import {
-  LiveblocksProvider, 
-  ClientSideSuspense, 
+  LiveblocksProvider,
+  ClientSideSuspense,
 } from "@liveblocks/react/suspense";
+
+const Provider = ({ children }: { children: ReactNode }) => {
+  const { user: clerkUser } = useUser();
+
+  return (
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth "
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+        return users;
+      }}
+    ></LiveblocksProvider>
+  );
+};
