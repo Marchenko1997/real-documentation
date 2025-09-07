8c49cb3b037abd2ab033cba0572464ecc57c867c
Halyna Marchenko <marchenkohalyna888@gmail.com>
2025-09-07 13:53:56 +0300

    correcting mistakes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter as FontSans } from "next/font/google";
 import { dark } from "@clerk/themes";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -36,7 +37,7 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
-          {children}
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>
